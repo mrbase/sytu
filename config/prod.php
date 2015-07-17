66e195ea53aede9d61db5baa62232f36f49436ad
ulrik nielsen <ulrik@o3.dk>
2015-07-17 23:46:40 +0200

    flush

--- config/prod.php
+++ config/prod.php
@@ -6,7 +6,7 @@ $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
 
 $app['spotify.options'] = [
-	'callback'  => 'http://sytu.un:9999/spotify/auth/callback',
-	'client_id' => '05da6a3361a04d6fa24b852327d3b667',
-	'secret'    => 'cd130cc914f948df839851a1f7f08b2c',
+	'callback'  => '',
+	'client_id' => '',
+	'secret'    => '',
 ];
